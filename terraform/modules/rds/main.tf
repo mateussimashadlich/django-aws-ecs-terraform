0cf254b2fab030ac8a74b6110c2388b8ed93db67
mateu <mateussimashadlich@gmail.com>
2026-02-11 21:31:27 +0100

    infra: uncomment deploy

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -35,11 +35,11 @@ resource "aws_db_instance" "production" {
   storage_type            = "gp2"
   port                    = "5432"
   engine                  = "postgres"
-  engine_version          = "15.4"
+  engine_version          = "18"
   instance_class          = "db.t4g.micro" # See instance pricing <https://aws.amazon.com/rds/postgres/pricing/?pg=pr&loc=2>
   multi_az                = false
   db_name                 = "mydatabase" # name is deprecated, use db_name instead
-  username                = "codewithmuh"
+  username                = "mateussh"
   skip_final_snapshot     = true
   publicly_accessible     = false
   backup_retention_period = 7
